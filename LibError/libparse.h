82ad9972ba74a1922e92060c61a16fa34e05ac62
Vanessa Lagardette <vanessa.lagardette@gmail.com>
2016-02-18 17:36:48 +0100

    new dossier

---
+++ LibError/libparse.h
@@ -0,0 +1,9 @@
+#ifndef _LIBERROR_H__
+#define _LIBERROR_H__
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+#endif
---
+++ LibParse/libparse.h
@@ -0,0 +1,9 @@
+#ifndef _LIBPARSE_H__
+#define _LIBPARSE_H__
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+#endif
